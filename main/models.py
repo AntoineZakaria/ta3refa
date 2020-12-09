acde445f00f83aac749a6df86c7f29efd908f77c
iamandrew <1700307@eng.asu.edu.eg>
2020-12-09 19:19:08 +0200

    person class 1

--- main/models.py
+++ main/models.py
@@ -1,3 +1,6 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Person(models.Model,User):
+    is_seller=models.BooleanField(default=False)
+    Purchased_products=models.ArrayField(models.TextField())
