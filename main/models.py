132595db565c9e7def59c3ef5a26c1463e60aac8
iamandrew <1700307@eng.asu.edu.eg>
2020-12-09 20:24:31 +0200

    person model(final version can be migrated)

--- main/models.py
+++ main/models.py
@@ -1,6 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import User
+
+from django.contrib.postgres.fields import ArrayField
 # Create your models here.
-class Person(models.Model,User):
+class Person(models.Model):
+    first_name=models.CharField(max_length=100)
+    last_name=models.CharField(max_length=100)
+    email=models.EmailField(max_length=100)
+    address=models.TextField(max_length=100)
     is_seller=models.BooleanField(default=False)
-    Purchased_products=models.ArrayField(models.TextField())
+    Purchased_products=ArrayField(models.CharField(max_length=100))
