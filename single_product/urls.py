1847e6ff1f878a1588a122944493f459d3f719cb
unknown <1701037@eng.asu.edu.eg>
2020-12-11 15:09:46 +0200

    update2

--- single_product/urls.py
+++ single_product/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns =[
-    path('', views.return_html_product , name='create_new_product'),
+    
     path('show_product/<id>', views.get_product , name='get_product'),
 ]
