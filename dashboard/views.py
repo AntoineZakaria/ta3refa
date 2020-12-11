cf61a007d486016aa6fd99cdcc8cda2dec09b457
iamandrew <1700307@eng.asu.edu.eg>
2020-12-11 13:33:15 +0200

    dashboard ready to add products

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,19 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from main.models import Product
 
 # Create your views here.
 def return_dashboard(request):
     return render(request,'dashboard form.html')
+
+
+
 def add_product(request):
     if request.method=='POST':
-        name=request.POST['product_name']
+        name=request.POST['name']
         category=request.POST['category']
-        description=request.POST['product_discription']
+        description=request.POST['discription']
         price=request.POST['price']
         quantity=request.POST['quantity']
         img=request.POST['img']
         offer=request.POST['offer']
-        new_product=Product(name=name,category=category,description=description,price=price,quantity=quantity,rate=0,offer=offer,comment="",img=img,shop_id=0)
+        new_product=Product(name=name,category=category,description=description,price=price,quantity=quantity,rate=0,offer=offer,comment=[],img=img,shop_id=0)
         new_product.save()
         return redirect('/')
         
