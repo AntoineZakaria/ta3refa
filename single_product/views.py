c3a84c0feda1c8065597b5004ea81f93354c131c
iamandrew <1700307@eng.asu.edu.eg>
2020-12-11 14:10:08 +0200

    11

--- single_product/views.py
+++ single_product/views.py
@@ -5,12 +5,9 @@ from main.models import Product
 def get_product(request,id):
     product = Product.objects.get(pk=id)
     
-<<<<<<< HEAD
 
 
-   pass
-=======
+  
     return render(request,'shop-detail.html',{'product':product})
->>>>>>> dabd41447f1509f16a865b4d3209bc5c28040080
 
     
