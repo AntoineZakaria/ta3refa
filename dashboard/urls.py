6f84dacbc2cc7cae14253177d5f408fa1d78f2c3
salmaehab <1700615@eng.asu.edu.eg>
2020-12-11 19:59:42 +0200

    edit_tempelate

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,7 +4,7 @@ urlpatterns =[
     path('', views.return_html_dashboard , name='return_dashboard'),
     path('add_product', views.add_product , name='add_product'),
     path('edit_product/<id>', views.edit_product , name='edit_product'),
-    path('tonyslamakero', views.return_edit_product , name='return_edit_product'),
+    path('edit_product_view', views.return_edit_product , name='return_edit_product'),
     path('add_shop', views.add_shop , name='add_shop'),
    
 ]
