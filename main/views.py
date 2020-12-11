cea286904b48faff06f538c82941d1de7f4e218e
salma39 <salmahamdy39@yahoo.co.uk>
2020-12-11 15:38:29 +0200

    return array random of products

--- main/views.py
+++ main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render ,redirect
 from django.contrib.auth.models import User , auth
 from .models import Product
 from . import views
+import random
 
 def return_offers():
     pass
@@ -20,9 +21,13 @@ def return_html_home (request):
     #return_random function
     #session user info
     """
-    # prods = Product.objects.all()
+    
+    #ya 4bab a3mlo add products ktyr 3l4an t4t8l
+    #array_of_random_pr=random_products(3)   
+    #prods = array_of_random_pr
+    prods=Product.objects.all()
 
-    return render(request,'shop-category-left.html')
+    return render(request,'shop-category-left.html',{'prods':prods})
 
 def login(request):
     pass
@@ -34,5 +39,21 @@ def return_offer():
 
 def signup(request):
     return render(request,'customer-register.html')
+
+def random_products(range_product):
+    #range_product -> number of products in the random products array
+    array_of_random_pr = []
+    for i in range(range_product):
+        flag = 1
+        while len(array_of_random_pr) < range_product:
+            random_object = Product.objects.all()[random.randint(0, Product.objects.count() - 1)]
+            for single_product in array_of_random_pr:
+                if single_product == random_object:
+                    flag = 0
+            if flag:        
+                array_of_random_pr.append(random_object)
+    return array_of_random_pr
+
+
 
 
