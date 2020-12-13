97e0402348d014cc932457ca1e80ad445d31b380
AntoineZakaria <tonyzakaria53@gmail.com>
2020-12-13 16:01:36 +0200

    another urls

--- single_product/urls.py
+++ single_product/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns =[
     
-    path('/show_product/<id>', views.get_product , name='get_product'),
+    path('show_product/<id>', views.get_product , name='get_product'),
 ]
