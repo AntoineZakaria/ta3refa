04f6b61b954e619e9ea8c378e0a982aebba27a6a
AntoineZakaria <tonyzakaria53@gmail.com>
2020-12-13 19:48:21 +0200

    RandomCategorizedProducts+first3 categories +some urls

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns =[
-    path('', views.return_html_home , name='home')
+    path('', views.return_html_home , name='home'),
+    path('shop_category/<category>', views.return_html_category, name='category')
    
    
 ]
