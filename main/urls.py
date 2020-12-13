e9a5f89b99dd946ddca9c57216b69df580e11cb9
AntwanEmil <antwanemile@gmail.com>
2020-12-13 19:06:15 +0200

    Revert "randomCategorizedObjects/phones category"
    This reverts commit 4ba83e4bc8ed886560d36448ad80f979efa00659.

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns =[
-    path('', views.return_html_home , name='home'),
-        path('shop_category/<category>', views.return_html_category, name='category')
-
+    path('', views.return_html_home , name='home')
    
    
 ]
