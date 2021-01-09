60bd208981e74db80073aa1316ab1db6dc726d7e
AntwanEmil <antwanemile@gmail.com>
2021-01-09 17:20:29 +0200

    rating & comments

--- single_product/views.py
+++ single_product/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
 from main.models import Product
+from django.contrib.auth.models import User,auth
 # Create your views here.
 
 
     
 def get_product(request,id):
     product = Product.objects.get(pk=id)
-   
 
 
   
     return render(request,'shop-detail.html',{'product':product})
 
     
+def review(request,id):
+    product = Product.objects.get(pk=id)
+    username= request.user.username
+    product.rate = 4
+    
+    product.comment.append([username,request.POST['comment']])
+    product.save()
+    return render(request,'shop-detail.html',{'product':product})
