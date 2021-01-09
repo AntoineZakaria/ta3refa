0d8bcd5f48ddc8e81eb53a86f9c2c11321be0ecb
AntwanEmil <antwanemile@gmail.com>
2021-01-09 21:13:10 +0200

    rate& comment

--- single_product/views.py
+++ single_product/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from main.models import Product
 from django.contrib.auth.models import User,auth
 from main.models import Person
+from django.http import HttpResponseRedirect
 # Create your views here.
 
 
@@ -23,8 +24,33 @@ def get_product(request,id):
 def review(request,id):
     product = Product.objects.get(pk=id)
     username= request.user.username
-    product.rate = 4
+    user_rate =0
     
-    product.comment.append([username,request.POST['comment']])
+    if "rating" in request.POST:
+        user_rate +=1
+    if "rating2" in request.POST:
+        user_rate =2
+    if "rating3" in request.POST:
+        user_rate =3
+    if "rating4" in request.POST:
+        user_rate =4
+    if "rating5" in request.POST:
+        user_rate =5
+
+    product.comment.append([username,int(user_rate),request.POST['comment']])
     product.save()
-    return render(request,'shop-detail.html',{'product':product})
+    ## calculate overall ratinggggggggggggggggg
+    prod = Product.objects.get(pk=id)
+    count = 0
+    rates = 0
+    produc_overall_rate =0
+
+    for comment in prod.comment:
+        one_rate = int(comment[1])
+        rates += one_rate
+        count +=1
+    produc_overall_rate = rates / count
+    prod.rate = produc_overall_rate
+    prod.save()
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
