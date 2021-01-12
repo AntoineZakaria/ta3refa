b8be33a93f5a69415f4b9de706c40f981de9e72f
AntoineZakaria <tonyzakaria53@gmail.com>
2021-01-12 20:36:55 +0200

    main views testing

--- main/test/test_views.py
+++ main/test/test_views.py
@@ -1,12 +1,52 @@
 from django.test import TestCase, Client
 from django.urls import reverse
 from main.models import Comment,Product,Person,Seller,Customer,mail_verification
+from django.contrib.auth.models import User,auth
 import json
 
 
 class TestMainViews (TestCase):
     def setUp(self):
         self.client=Client()
+        User.objects.create_user(
+            id=1,
+            username='tony',
+            email='tony.felo18@gmail.com',
+            password='1234',
+            is_superuser=True,
+            first_name='antonios',
+            last_name='amgad',
+            is_staff=True,
+            is_active=True,
+            date_joined='2021-01-11 16:49:26.898785+02'
+        )
+        Person.objects.create(
+            id=1,
+            username='tony',
+            address='ADDRESS1',
+            is_seller=True,
+            Purchased_products=[],
+            favourite_products=[]
+        )
+        Seller.objects.create(
+            id=1,
+            username='tony',
+            shop_name = 'shop1_test',
+            telephone = 123, 
+            owned_products = [],
+            current_balance = 0
+        )
+        mail_verification.objects.create(
+        user_name= 'tony',
+        message_code='TEST',
+        is_autonticated=True
+        )
+
+        self.url_login=reverse('login')
+        response=self.client.post(self.url_login ,{
+            'email_login':'tony',
+            'password_login':'1234'
+        })
         self.url_home=reverse('home')#
         self.url_verify_code=reverse('verify_code',args=['tonytonytony'])#
         self.url_category=reverse('category',args=['hiiiii'])
