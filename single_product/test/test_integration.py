b58a752b36a3a398210c5985147627fcac8a4c9b
tony-amgad <1700321@eng.asu.edu.eg>
2021-01-12 22:08:00 +0200

    edit

---
+++ single_product/test/test_integration.py
@@ -0,0 +1,84 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+from main.models import Comment,Product,Person,Seller,Customer,mail_verification
+from django.contrib.auth.models import User,auth
+import json
+
+
+class TestSingle_productViews (TestCase):
+    def setUp(self):
+        self.client=Client()
+        self.url_get_product=reverse('get_product',args=[1])
+        self.url_review = reverse('review' , args=[1])
+        self.client=Client()
+        User.objects.create_user(
+            username='tony',
+            email='tony.felo18@gmail.com',
+            password='1234',
+            is_superuser=False,
+            first_name='antonios',
+            last_name='amgad',
+            is_staff=False,
+            is_active=True
+        )
+        Person.objects.create(
+            username='tony',
+            address='ADDRESS1',
+            is_seller=True,
+            Purchased_products=[],
+            favourite_products=[]
+        )
+        Seller.objects.create(
+            username='tony',
+            shop_name = 'shop1_test',
+            telephone = 123, 
+            owned_products = [],
+            current_balance = 0
+        )
+
+        Product.objects.create(
+            id=1,
+            name="prod",
+            description="desc",
+            price= 100,
+            quantity=5,
+            rate = 3,
+            offer = 1,
+            img= "product_image/bridge_dVtWd1W.jpg",
+            comment = [['tony',1,"good"]],
+            shop_id="1",
+            category="Phones"
+        )
+        self.url_login=reverse('login')
+        response=self.client.post(self.url_login,{
+            'email_login':'tony',
+            'password_login':'1234',
+        }
+        )
+        self.assertEquals(response.status_code,302)
+        response=self.client.get(self.url_get_product)
+        self.assertEquals(response.status_code,200)
+        self.assertTemplateUsed(response,'shop-detail.html')
+        #giving the product rate=1
+        response = self.client.post(self.url_review,{'username':"tony",'comment':"bad",'rating':True})
+        self.assertEquals(response.status_code,302)
+        #testing the overall rating 
+        self.assertEquals(Product.objects.get(name='prod').rate , 1)
+        #Testing If the comment has been added proberly
+        self.assertEquals(Product.objects.get(name='prod').comment[1][1] , '1')
+        self.assertEquals(Product.objects.get(name='prod').comment[1][2] , "bad")
+        Product.objects.create(
+            id=10,
+            name="prod10",
+            description="desc",
+            price= 100,
+            quantity=5,
+            rate = 3,
+            offer = 50,
+            img= "product_image/bridge_dVtWd1W.jpg",
+            comment = [],
+            shop_id="1",
+            category="Phones"
+        )
+        self.assertEquals(Product.objects.get(id=10).new_price(),50)
+         
